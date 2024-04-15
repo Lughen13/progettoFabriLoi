cf715475588d7fd805739c4feed48aafa6f97307
Lughen13 <l.loi1@studenti.unipi.it>
2024-04-15 15:39:59 +0200

    asdsf

--- register.php
+++ register.php
@@ -1,1 +1,15 @@
 <h1>hello world!</h1>
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+
+    // Aggiungi qui i controlli sugli input
+
+    $query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("sss", $username, $email, $password);
+    $stmt->execute();
