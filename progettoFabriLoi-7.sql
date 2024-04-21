02c312d49e2eac73845cfcef8d82ceb1fb580f1d
Lughen13 <l.loi1@studenti.unipi.it>
2024-04-21 12:46:32 +0200

    test

--- progettoFabriLoi-7.sql
+++ progettoFabriLoi-7.sql
@@ -1,101 +1,399 @@
-<?php
-// Connessione DB
-$host = "localhost";
-$user = "root"; 
-$password = "";
-$db = "progettoFabriLoi";
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Creato il: Apr 20, 2024 alle 18:44
+-- Versione del server: 10.4.28-MariaDB
+-- Versione PHP: 8.2.4
 
-$conn = mysqli_connect($host, $user, $password, $db);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-if(!$conn){
-    die("Connessione fallita: " . mysqli_connect_error());
-}
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-  $username = $_POST['username'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
+--
+-- Database: `progettoFabriLoi`
+--
 
-  $nome = $_POST['nome'];
-  $cognome = $_POST['cognome'];
-  $genere = $_POST['genere'];
-  $data_nascita = $_POST['data_nascita'];
+-- --------------------------------------------------------
 
-  $password_hash = password_hash($password, PASSWORD_DEFAULT);  
+--
+-- Struttura della tabella `blog`
+--
 
-  $premium = isset($_POST['premium']) ? 1 : 0;
-  $intestatario = $_POST['intestatario'];
-  $numero_carta = $_POST['numero_carta'];
-  $data_scadenza = $_POST['data_scadenza'];
-  $cvv = $_POST['cvv'];
+CREATE TABLE `blog` (
+  `id_blog` int(10) NOT NULL,
+  `data_blog` datetime NOT NULL,
+  `titolo_blog` varchar(50) NOT NULL,
+  `descrizione` text CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `img_logo` varchar(50) NOT NULL DEFAULT 'default.png',
+  `id_categoria` int(10) NOT NULL,
+  `id_stile` int(10) NOT NULL,
+  `id_proprietario` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-  $sql = "INSERT INTO utente (username, email, pw, nome, cognome, genere, data_nascita, premium, intestatario, numero_carta, data_scadenza, cvv)
-          VALUES ('$username', '$email', '$password_hash', '$nome', '$cognome', '$genere', '$data_nascita', $premium, '$intestatario', '$numero_carta', '$data_scadenza', '$cvv')";
-  
-  if(mysqli_query($conn, $sql)){
-    echo "Registrazione avvenuta con successo!";
-  } else {
-    echo "Errore: " . mysqli_error($conn);
-  }
+-- --------------------------------------------------------
 
-}
+--
+-- Struttura della tabella `categoria`
+--
 
-?>
+CREATE TABLE `categoria` (
+  `id_categoria` int(20) NOT NULL,
+  `descrizione` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Registrazione</title>
-</head>
-<body>
+-- --------------------------------------------------------
 
-  <h1>Registrazione</h1>
+--
+-- Struttura della tabella `commento`
+--
 
-  <form method="post">
+CREATE TABLE `commento` (
+  `id_comm` int(10) NOT NULL,
+  `data_comm` datetime NOT NULL,
+  `contenuto` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `id_utente` int(10) NOT NULL,
+  `id_post` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username"><br><br>
+--
+-- Trigger `commento`
+--
+DELIMITER $$
+CREATE TRIGGER `update_comment_count` AFTER INSERT ON `commento` FOR EACH ROW UPDATE post
+SET comments = comments + 1
+WHERE post.id = post_id
+$$
+DELIMITER ;
 
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email"><br><br>
+-- --------------------------------------------------------
 
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password"><br><br>
+--
+-- Struttura della tabella `co_autore`
+--
 
-    <label for="nome">Nome:</label>
-    <input type="text" id="nome" name="nome"><br><br>
+CREATE TABLE `co_autore` (
+  `id_utente` int(10) NOT NULL,
+  `id_blog` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-    <label for="cognome">Cognome:</label>
-    <input type="text" id="cognome" name="cognome"><br><br>
+-- --------------------------------------------------------
 
-    <label for="genere">Genere:</label>
-    <select id="genere" name="genere">
-      <option value="M">Maschio</option>
-      <option value="F">Femmina</option>
-    </select><br><br>
+--
+-- Struttura della tabella `follow`
+--
 
-    <label for="data_nascita">Data di nascita:</label>
-    <input type="date" id="data_nascita" name="data_nascita"><br><br>
+CREATE TABLE `follow` (
+  `id_follow` int(10) NOT NULL,
+  `id_utente` int(10) NOT NULL,
+  `id_blog` int(10) NOT NULL,
+  `data_follow` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-    <label for="premium">Utente Premium:</label>
-    <input type="checkbox" id="premium" name="premium"><br><br>
+--
+-- Trigger `follow`
+--
+DELIMITER $$
+CREATE TRIGGER `after_follow_insert` AFTER INSERT ON `follow` FOR EACH ROW BEGIN
+   UPDATE `blog`
+   SET `followers_count` = `followers_count` + 1
+   WHERE `id_blog` = NEW.`id_blog`;
+END
+$$
+DELIMITER ;
 
-    <label for="intestatario">Intestatario:</label>
-    <input type="text" id="intestatario" name="intestatario"><br><br>
+-- --------------------------------------------------------
 
-    <label for="numero_carta">Numero carta:</label>
-    <input type="text" id="numero_carta" name="numero_carta"><br><br>
+--
+-- Struttura della tabella `like`
+--
 
-    <label for="data_scadenza">Data di scadenza:</label>
-    <input type="date" id="data_scadenza" name="data_scadenza"><br><br>
+CREATE TABLE `like` (
+  `id_like` int(10) NOT NULL,
+  `id_utente` int(10) NOT NULL,
+  `id_post` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-    <label for="cvv">CVV:</label>
-    <input type="text" id="cvv" name="cvv"><br><br>
+--
+-- Trigger `like`
+--
+DELIMITER $$
+CREATE TRIGGER `after_like_insert` AFTER INSERT ON `like` FOR EACH ROW BEGIN
+   UPDATE `post`
+   SET `likes_count` = `likes_count` + 1
+   WHERE `id_post` = NEW.`id_post`;
+END
+$$
+DELIMITER ;
 
-    <input type="submit" value="Registrati">
+-- --------------------------------------------------------
 
-  </form>
+--
+-- Struttura della tabella `post`
+--
 
-</body>
-</html>
+CREATE TABLE `post` (
+  `id_post` int(10) NOT NULL,
+  `data_post` datetime NOT NULL,
+  `titolo_post` varchar(50) NOT NULL,
+  `descrizione_post` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `img_post` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'default.png',
+  `id_autore` int(10) NOT NULL,
+  `id_sotcat` int(10) NOT NULL,
+  `id_blog` int(10) NOT NULL,
+  `likes_count` int(10) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `sottocat`
+--
+
+CREATE TABLE `sottocat` (
+  `id_sottocat` int(10) NOT NULL,
+  `titolo` varchar(20) NOT NULL,
+  `id_categoria` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `stile`
+--
+
+CREATE TABLE `stile` (
+  `id_stile` int(10) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `font` varchar(20) NOT NULL,
+  `colore_testo` varchar(10) NOT NULL,
+  `background` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `utente`
+--
+
+CREATE TABLE `utente` (
+  `id_utente` int(10) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `pw` varchar(40) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `img_profilo` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'default.png',
+  `nome` varchar(20) NOT NULL,
+  `cognome` varchar(20) NOT NULL,
+  `genere` varchar(10) NOT NULL,
+  `data_nascita` date NOT NULL,
+  `bio` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `numero_telefono` varchar(10) NOT NULL,
+  `premium` tinyint(1) NOT NULL DEFAULT 0,
+  `intestatario` varchar(40) NOT NULL,
+  `carta` varchar(16) NOT NULL,
+  `data_scadenza` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `blog`
+--
+ALTER TABLE `blog`
+  ADD PRIMARY KEY (`id_blog`),
+  ADD UNIQUE KEY `id_blog` (`id_blog`,`titolo_blog`),
+  ADD UNIQUE KEY `titolo_blog` (`titolo_blog`,`id_proprietario`),
+  ADD KEY `id_categoria` (`id_categoria`,`id_stile`,`id_proprietario`),
+  ADD KEY `id_proprietario` (`id_proprietario`),
+  ADD KEY `blog_ibfk_2` (`id_stile`);
+
+--
+-- Indici per le tabelle `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indici per le tabelle `commento`
+--
+ALTER TABLE `commento`
+  ADD PRIMARY KEY (`id_comm`),
+  ADD KEY `id_utente` (`id_utente`,`id_post`),
+  ADD KEY `id_post` (`id_post`);
+
+--
+-- Indici per le tabelle `co_autore`
+--
+ALTER TABLE `co_autore`
+  ADD PRIMARY KEY (`id_utente`,`id_blog`),
+  ADD KEY `id_utente` (`id_utente`,`id_blog`),
+  ADD KEY `id_blog` (`id_blog`);
+
+--
+-- Indici per le tabelle `follow`
+--
+ALTER TABLE `follow`
+  ADD PRIMARY KEY (`id_follow`),
+  ADD UNIQUE KEY `id_utente` (`id_utente`,`id_blog`),
+  ADD KEY `id_follow` (`id_follow`,`id_utente`,`id_blog`);
+
+--
+-- Indici per le tabelle `like`
+--
+ALTER TABLE `like`
+  ADD PRIMARY KEY (`id_like`),
+  ADD UNIQUE KEY `id_utente_2` (`id_utente`,`id_post`),
+  ADD KEY `id_utente` (`id_utente`,`id_post`),
+  ADD KEY `id_post` (`id_post`);
+
+--
+-- Indici per le tabelle `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id_post`),
+  ADD KEY `id_autore` (`id_autore`,`id_sotcat`,`id_blog`),
+  ADD KEY `id_blog` (`id_blog`),
+  ADD KEY `id_sotcat` (`id_sotcat`);
+
+--
+-- Indici per le tabelle `sottocat`
+--
+ALTER TABLE `sottocat`
+  ADD PRIMARY KEY (`id_sottocat`),
+  ADD KEY `id_post` (`id_categoria`) USING BTREE,
+  ADD KEY `titolo` (`titolo`);
+
+--
+-- Indici per le tabelle `stile`
+--
+ALTER TABLE `stile`
+  ADD PRIMARY KEY (`id_stile`);
+
+--
+-- Indici per le tabelle `utente`
+--
+ALTER TABLE `utente`
+  ADD PRIMARY KEY (`id_utente`),
+  ADD UNIQUE KEY `username` (`username`,`email`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `blog`
+--
+ALTER TABLE `blog`
+  MODIFY `id_blog` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `commento`
+--
+ALTER TABLE `commento`
+  MODIFY `id_comm` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `like`
+--
+ALTER TABLE `like`
+  MODIFY `id_like` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `post`
+--
+ALTER TABLE `post`
+  MODIFY `id_post` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `sottocat`
+--
+ALTER TABLE `sottocat`
+  MODIFY `id_sottocat` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `stile`
+--
+ALTER TABLE `stile`
+  MODIFY `id_stile` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `utente`
+--
+ALTER TABLE `utente`
+  MODIFY `id_utente` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `blog`
+--
+ALTER TABLE `blog`
+  ADD CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`),
+  ADD CONSTRAINT `blog_ibfk_2` FOREIGN KEY (`id_stile`) REFERENCES `stile` (`id_stile`),
+  ADD CONSTRAINT `blog_ibfk_3` FOREIGN KEY (`id_proprietario`) REFERENCES `utente` (`id_utente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `blog_ibfk_4` FOREIGN KEY (`id_blog`) REFERENCES `co_autore` (`id_blog`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `commento`
+--
+ALTER TABLE `commento`
+  ADD CONSTRAINT `commento_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `commento_ibfk_2` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id_utente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `commento_ibfk_3` FOREIGN KEY (`id_comm`) REFERENCES `notifica` (`id_notifica`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `follow`
+--
+ALTER TABLE `follow`
+  ADD CONSTRAINT `follow_ibfk_1` FOREIGN KEY (`id_follow`) REFERENCES `blog` (`id_blog`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `follow_ibfk_2` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id_utente`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `like`
+--
+ALTER TABLE `like`
+  ADD CONSTRAINT `like_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `like_ibfk_2` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id_utente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `like_ibfk_3` FOREIGN KEY (`id_like`) REFERENCES `notifica` (`id_notifica`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`id_blog`) REFERENCES `blog` (`id_blog`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_ibfk_2` FOREIGN KEY (`id_autore`) REFERENCES `utente` (`id_utente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_ibfk_3` FOREIGN KEY (`id_sotcat`) REFERENCES `sottocat` (`id_sottocat`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `sottocat`
+--
+ALTER TABLE `sottocat`
+  ADD CONSTRAINT `sottocat_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `utente`
+--
+ALTER TABLE `utente`
+  ADD CONSTRAINT `utente_ibfk_1` FOREIGN KEY (`id_utente`) REFERENCES `co_autore` (`id_utente`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
