66fd52d2f761ae4f1e8d39dbdbb018bcab14467b
lughen13 <l.loi1@studenti.unipi.it>
2024-06-19 15:19:33 +0200

    piccoli cambiamenti

--- process_create_post.php
+++ process_create_post.php
@@ -19,15 +19,31 @@ $title = $_POST['title'];
 $description = $_POST['description'];
 $subcategoryId = $_POST['subcategory'];
 
-// immagine ancora da gestire 
-$imageName = '';
-if (!empty($_FILES['image']['name'])) {
-    $imageTmp = $_FILES['image']['tmp_name'];
-    $imageName = 'uploads/' . uniqid() . '_' . $_FILES['image']['name'];
-    move_uploaded_file($imageTmp, $imageName);
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
+    $imageFile = $_FILES['image'];
+    $imageTmpName = $imageFile['tmp_name'];
+    $imageFileName = $imageFile['name'];
+    $imageSize = $imageFile['size'];
+    $imageError = $imageFile['error'];
+
+    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+    $imageExtension = strtolower(pathinfo($imageFileName, PATHINFO_EXTENSION));
+
+    if (in_array($imageExtension, $allowedExtensions) && $imageError === 0) {
+        $imageName = $title . '.' . $imageExtension; // Nuovo nome del file del logo
+        $imageDestination = 'photo_post/' . $imageName;
+
+        if (move_uploaded_file($imageTmpName, $imageDestination)) {
+            echo "immagine caricata con successo.";
+        } else {
+            echo "Errore durante il caricamento dell'immagine.";
+        }
+    } else {
+        echo "Formato dell'immagine non valido o errore durante il caricamento.";
+    }
 }
 
-// inserisco il nuovo post all'interno del db
+// Inserisci il nuovo post nel database
 $sql = "INSERT INTO post (titolo_post, descrizione_post, img_post, id_autore, id_sottocat, id_blog, data_post) VALUES (?, ?, ?, ?, ?, ?, NOW())";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("sssiii", $title, $description, $imageName, $userId, $subcategoryId, $blogId);
