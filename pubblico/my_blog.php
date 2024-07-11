ce484c0c7d4b4261bea2dd3d2cbae8e8c2d14422
Fefina1 <federicafabri11@gmail.com>
2024-07-11 20:53:32 +0200

    ECHOOOO

--- pubblico/my_blog.php
+++ pubblico/my_blog.php
@@ -434,31 +434,30 @@ while ($row = $resultLikes->fetch_assoc()) {
                                             </div>
 
                                             <div class="mt-3">
-                            <?php
-                            $totalLikes = isset($likeCounts[$post['id_post']]) ? $likeCounts[$post['id_post']] : 0;
-                            $likeAction = 'like';
-                            if ($_SESSION['loggedin'] === true) {
-                                include '../configurazione/conn.php';
+                                                <form class="like-form">
+                                                    <input type="hidden" class="post-id" value="<?php echo $post['id_post']; ?>">
+                                                    <input type="hidden" class="id-blog" value="<?php echo $id_blog; ?>">
+                                                    <?php
+                                                    $likeAction = 'like'; // Default to 'like' if user has not liked the post yet
+                                                    if ($_SESSION['loggedin'] === true) {
+                                                        include '../configurazione/conn.php';
 
-                                $likeQuery = "SELECT * FROM likes WHERE id_post = ? AND id_utente = ?";
-                                $stmt = $conn->prepare($likeQuery);
-                                $stmt->bind_param("ii", $post['id_post'], $userId);
-                                $stmt->execute();
-                                $likeResult = $stmt->get_result();
-                                if ($likeResult->num_rows > 0) {
-                                    $likeAction = 'unlike';
-                                }
-                                $stmt->close();
-                            }
-                            ?>
-                            <!-- Form per Mi Piace -->
-                            <form class="like-form">
-                                <input type="hidden" class="post-id" value="<?php echo $post['id_post']; ?>">
-                                <input type="hidden" class="id-blog" value="<?php echo $id_blog; ?>">
-                                <button type="button" class="btn btn-success like-btn" data-action="<?php echo $likeAction; ?>">
-                                    Mi Piace (<span class="like-count"><?php echo $totalLikes; ?></span>)
-                                </button>
-                            </form>
+                                                        $likeQuery = "SELECT * FROM likes WHERE id_post = ? AND id_utente = ?";
+                                                        $stmt = $conn->prepare($likeQuery);
+                                                        $stmt->bind_param("ii", $post['id_post'], $userId);
+                                                        $stmt->execute();
+                                                        $likeResult = $stmt->get_result();
+                                                        if ($likeResult->num_rows > 0) {
+                                                            $likeAction = 'unlike'; // If user has liked the post, set action to 'unlike'
+                                                        }
+                                                        $stmt->close();
+                                                    }
+                                                    ?>
+                                                    <button type="button" class="btn btn-success like-btn" data-action="<?php echo $likeAction; ?>">
+                                                        <?php echo $likeAction === 'like' ? 'Mi Piace' : 'Togli Mi Piace'; ?>
+                                                    </button>
+                                                </form>
+                                            </div>
 
                                             <!-- Recupero dei commenti per questo post -->
                                             <?php
@@ -609,17 +608,18 @@ while ($row = $resultLikes->fetch_assoc()) {
     }
 
     $(document).ready(function() {
-    $('.edit-comment-btn').click(function() {
-        var commentId = $(this).data('comment-id');
-        $(this).closest('.card-body').find('.comment-text').hide();
-        $(this).closest('.card-body').find('.edit-comment-form').removeClass('d-none');
-    });
+    // Funzione per aggiornare il testo del pulsante Mi Piace
+    function updateLikeButton(button, action, likeCount) {
+        if (action === 'like') {
+            button.data('action', 'unlike');
+            button.text('Togli Mi Piace (' + likeCount + ')');
+        } else {
+            button.data('action', 'like');
+            button.text('Mi Piace (' + likeCount + ')');
+        }
+    }
 
-    $('.cancel-edit-btn').click(function() {
-        $(this).closest('.edit-comment-form').addClass('d-none');
-        $(this).closest('.card-body').find('.comment-text').show();
-    });
-
+    // Gestione del clic sul pulsante Mi Piace
     $('.like-btn').click(function() {
         var button = $(this);
         var postId = button.closest('.like-form').find('.post-id').val();
@@ -634,13 +634,29 @@ while ($row = $resultLikes->fetch_assoc()) {
             },
             success: function(likeCount) {
                 // Aggiorna il testo del pulsante e il conteggio dei Mi Piace dinamicamente
-                if (action === 'like') {
-                    button.data('action', 'unlike');
-                    button.text('Togli Mi Piace (' + likeCount + ')');
-                } else {
-                    button.data('action', 'like');
-                    button.text('Mi Piace (' + likeCount + ')');
-                }
+                updateLikeButton(button, action, likeCount);
+            },
+            error: function(xhr, status, error) {
+                console.error('Errore durante l\'invio della richiesta AJAX: ' + error);
+            }
+        });
+    });
+
+    // Chiamata iniziale per aggiornare il testo del pulsante per tutti i Mi Piace
+    $('.like-btn').each(function() {
+        var button = $(this);
+        var postId = button.closest('.like-form').find('.post-id').val();
+
+        $.ajax({
+            url: '../risorse/likes.php',
+            type: 'POST',
+            data: {
+                post_id: postId,
+                action: 'get_like_count' // Chiamata per ottenere il conteggio iniziale dei Mi Piace
+            },
+            success: function(likeCount) {
+                // Aggiorna il testo del pulsante con il conteggio iniziale dei Mi Piace
+                updateLikeButton(button, 'like', likeCount);
             },
             error: function(xhr, status, error) {
                 console.error('Errore durante l\'invio della richiesta AJAX: ' + error);
@@ -648,6 +664,7 @@ while ($row = $resultLikes->fetch_assoc()) {
         });
     });
 });
+
 </script>
 </body>
 </html>
